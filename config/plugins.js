60d7ad12e8c2961f4f03c8f067488a8d77f9ec4e
rafiq9190 <muhammadrafiq9190@gmail.com>
2023-04-16 12:57:46 +0500

    code update

--- config/plugins.js
+++ config/plugins.js
@@ -1,19 +1,45 @@
 module.exports = ({ env }) => ({
-    // ...
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          uploadStream: {},
-          delete: {},
-        },
+  // ...
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
-    // ...
-  });
+  },
+  editor: {
+    plugins: [
+      "Heading",
+      "Bold",
+      "Italic",
+      "Link",
+      "BulletedList",
+      "NumberedList",
+      "ImageUpload",
+    ],
+    toolbar: [
+      "heading",
+      "|",
+      "bold",
+      "italic",
+      "link",
+      "|",
+      "bulletedList",
+      "numberedList",
+      "|",
+      "imageUpload",
+      "|",
+      "undo",
+      "redo",
+    ],
+  },
+  // ...
+});
